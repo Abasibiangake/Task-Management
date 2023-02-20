3e187f2e2ab9372ab82764e318c6fd9c5246d142
Abasibiangake <48968451+Abasibiangake@users.noreply.github.com>
2023-02-19 19:06:38 -0500

    Full Stack MERN

---
+++ config/env/development.js
@@ -0,0 +1,6 @@
+//Development configuration options
+//To sign the session identifier, use a secret string
+module.exports = {
+    db: 'mongodb://127.0.0.1/sample-w23-db',
+    sessionSecret: 'developmentSession-nSecret'
+};
