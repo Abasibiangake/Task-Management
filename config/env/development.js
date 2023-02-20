b8013840434552cc1ed77e98642ee4b55f2b60b9
Abasibiangake James <jamesabasibiangake@gmail.com>
2023-02-19 19:12:10 -0500

    Revert "Full Stack MERN"
    This reverts commit 3e187f2e2ab9372ab82764e318c6fd9c5246d142.

--- config/env/development.js
+++
@@ -1,6 +0,0 @@
-//Development configuration options
-//To sign the session identifier, use a secret string
-module.exports = {
-    db: 'mongodb://127.0.0.1/sample-w23-db',
-    sessionSecret: 'developmentSession-nSecret'
-};
