6e5b5bd97c3fe635ddc85e014a5dec3017914f33
Abasibiangake James <jamesabasibiangake@gmail.com>
2023-02-19 19:13:49 -0500

    Revert "Full Stack MERN"
    This reverts commit 3e187f2e2ab9372ab82764e318c6fd9c5246d142.

--- app/controllers/index.server.controller.js
+++
@@ -1,7 +0,0 @@
-// Create a new 'render' controller method
-exports.render = function (req, res) {
-    // Use the 'response' object to render the 'index' view with a 'title' property
-    res.render('index', {title: 'Sample Test W23'} );
-    
-};
-
